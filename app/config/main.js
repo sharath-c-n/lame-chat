ed33b0a7c761ddfd37505a265983782c02fa90da
sharath-c-n <sharathcn@outlook.com>
2016-12-23 18:22:21 -0600

    Update main.js
    Updating files

--- app/config/main.js
+++ app/config/main.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret':'dnjksydqu4i237r7gdjwaku2346iowefh',
-     'database':'mongodb://sharath:sharath1@ds031607.mlab.com:31607/auth',
-    // 'database':'mongodb://localhost:27017/auth',
+    //Add your database URL here
+    'database':'mongodb://localhost:27017/auth',
     'defChatId' :'96cf91bdc3464f11878934fb'
 };
